1d7cb208bb55edcf2677a480a954f1c774e0083d
Nagaraj-Halatti <84122827+Nagaraj-Halatti@users.noreply.github.com>
2022-09-03 00:27:07 +0530

    Create warshall.c

---
+++ warshall.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int a[30][30];
+void warshall(int n){
+ for(int k=1;k<=n;k++)
+ for(int i=1;i<=n;i++)
+ for(int j=1;j<=n;j++)
+ a[i][j]=a[i][j]|| (a[i][k] && a[k][j]);
+}
+int main(){
+ int n;
+ printf("Enter no of vertices: \n");
+ scanf("%d",&n);
+ printf("Enter adjacency matrix: \n");
+ for(int i=1;i<=n;i++)
+ for(int j=1;j<=n;j++)
+ scanf("%d",&a[i][j]);
+ warshall(n);
+ printf("Transitive Closure: \n");
+ for(int i=1;i<=n;i++){
+ for(int j=1;j<=n;j++)
+ printf("%d ",a[i][j]);
+ printf("\n");
+ }
+ int flag = 0;
+ for(int i=1;i<=n;i++){
+ for(int j=1;j<=n;j++){
+ if (i==j)
+ if(a[i][j]==1)
+ flag = 1;
+ }
+ }
+ if(flag==0)
+ printf("Cycle present");
+ else
+ printf("No Cycle ");
+}
